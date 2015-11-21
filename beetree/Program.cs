c17512f815b2bb0a4581a550b5bddfc482a1a5aa
Parikshit Misra <perryizgr8@live.in>
2015-11-22 00:04:30 +0530

    Base project

---
+++ beetree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace beetree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("huh");
+            System.Console.ReadKey(true);
+        }
+    }
+    class NodeOfBtrie
+    {
+
+    }
+}
