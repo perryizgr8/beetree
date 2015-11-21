b5a72cb8325e1b3a78e63cf01401de2cd2fd798b
Parikshit Misra <perryizgr8@live.in>
2015-11-22 01:42:16 +0530

    Data structures made, need methods now

--- beetree/Program.cs
+++ beetree/Program.cs
@@ -1,15 +1,48 @@
-﻿namespace beetree
+﻿using System;
+using System.Collections.Generic;
+namespace beetree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("huh");
-            System.Console.ReadKey(true);
+            Console.WriteLine("B tree by perryizgr8");
+            Console.ReadKey(true);
         }
     }
-    class NodeOfBtrie
+    class Node<A, B>
     {
-
+        List<KeyValuePair<int, IntPtr>> Keys;
+        List<Node<A, B>> Children;
+        public Node()
+        {
+            Keys = new List<KeyValuePair<int, IntPtr>>();
+            Children = new List<Node<A, B>>();
+        }
+        public int getNumberOfKeys()
+        {
+            return Keys.Count;
+        }
+        public bool isLeaf()
+        {
+            if (Children.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Btrie<A, B>
+    {
+        Node<A, B> rootNode;
+        int degree;
+        public Btrie(int degree)
+        {
+            if(degree < 2)
+            {
+                throw new System.ArgumentOutOfRangeException("degree", "Degree must be at least 2.");
+            }
+            this.degree = degree;
+            rootNode = new Node<A, B>();
+        }
     }
 }
